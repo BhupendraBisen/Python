1a7b79f655bbf44e0b2209942d60df94a968e203
Bhupendra Bisen <128673113+BhupendraBisen@users.noreply.github.com>
2023-03-23 13:22:15 +0530

    Add files via upload

---
+++ 38_area_of_circle.py
@@ -0,0 +1,6 @@
+# Find the area of the given circle using the math module.
+
+import math as M
+Radius = float (input ("Please enter the radius of the given circle: "))
+area_of_the_circle = M.pi* Radius * Radius
+print (" The area of the given circle is: ", area_of_the_circle)
