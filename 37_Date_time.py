81b0725845c71900e001b775513ed7836bb1966e
Bhupendra Bisen <128673113+BhupendraBisen@users.noreply.github.com>
2023-03-23 13:10:16 +0530

    Add files via upload

---
+++ 37_Date_time.py
@@ -0,0 +1,18 @@
+import datetime
+
+X = datetime.datetime(2018,6,1)
+
+print(X.strftime("%d"))
+print(X.strftime("%Y"))
+print(X.strftime("%b"))
+print(X.strftime("%c"))
+
+from datetime import datetime
+from datetime import timedelta
+
+#taking input as the date
+Begindatestring = "2020-10-11"
+
+#carry out conversion between string
+#to datetime object
+Begindate = datetime.strptime(Begindatestring, "%Y-%m-%d")
